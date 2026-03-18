01541ed1d2f4565b4b88eafbb229a8d8e1c8af1a
Savean Osman <111705103+Savean05@users.noreply.github.com>
2026-03-17 18:03:46 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,471 +1,302 @@
-#-------imports-------
+# -------imports-------
 import turtle as trtl
-import turtle as bird
 import random as ran
 import time as times
+import os
+from PIL import Image
 
 
-#-------setting background and images-------
-bird.speed(0)
+# -------High Score Logic-------
+def get_high_score(mode):
+    filename = f"highscore_{mode}.txt"
+    if not os.path.exists(filename):
+        return 0
+    with open(filename, "r") as f:
+        try:
+            return int(f.read())
+        except:
+            return 0
+
+
+def save_high_score(new_score, mode):
+    high_score = get_high_score(mode)
+    if new_score > high_score:
+        filename = f"highscore_{mode}.txt"
+        with open(filename, "w") as f:
+            f.write(str(new_score))
+        return True
+    return False
+
+
+# -------Screen Setup & Dynamic Background-------
 wn = trtl.Screen()
 wn.tracer(False)
 wn.setup(width=1.0, height=1.0)
-pipeSpeed = 3
-wn.bgpic("background.gif")
-pipe = "pipe.gif"
-pipe2 = "pipe2.gif"
-back2 = "background2.gif"
-redbird = "redbird.gif"
-bluebird = "bluebird.gif"
-yellowbird = "yellowbird.gif"
-explosion = "explosion.gif"
-redbird2 = "redbirdfall.gif"
-redbirdfall = "redbirdfall.gif"
-yellowbirdfall = "yellowbirdfall.gif"
-bluebirdfall = "bluebirdfall.gif"
-redbirdup = "redbirdup.gif"
-bluebirdup = "bluebirdup.gif"
-yellowbirdup = "yellowbirdup.gif"
-wn.addshape(explosion)
-wn.addshape(yellowbird)
-wn.addshape(bluebird)
-wn.addshape(redbird)
-wn.addshape(back2)
-wn.addshape(pipe)
-wn.addshape(pipe2)
-wn.addshape(redbird2)
-wn.addshape(bluebirdfall)
-wn.addshape(yellowbirdfall)
-wn.addshape(redbirdfall)
-wn.addshape(redbirdup)
-wn.addshape(bluebirdup)
-wn.addshape(yellowbirdup)
-#-------setting up score writing-------
-font_setup = ("Times New Roman", 30, "normal")
-font_setup2 = ("Times New Roman", 70, "normal")
+
+try:
+    wn._root().state('zoomed')
+except:
+    pass
+
+screen_width = wn.window_width()
+screen_height = wn.window_height()
+
+try:
+    original_bg = Image.open("background.gif")
+    stretched_bg = original_bg.resize((screen_width, screen_height), Image.Resampling.LANCZOS)
+    stretched_bg.save("temp_bg.gif")
+    wn.bgpic("temp_bg.gif")
+except:
+    wn.bgpic("background.gif")
+
+# Shapes
+shapes = ["pipe.gif", "pipe2.gif", "redbird.gif", "bluebird.gif",
+          "yellowbird.gif", "explosion.gif", "redbirdfall.gif", "yellowbirdfall.gif",
+          "bluebirdfall.gif", "redbirdup.gif", "bluebirdup.gif", "yellowbirdup.gif"]
+for s in shapes:
+    wn.addshape(s)
+
+# -------Turtles & UI-------
+font_setup = ("Times New Roman", 35, "bold")
+font_setup2 = ("Times New Roman", 70, "bold")
 font_setup3 = ("Times New Roman", 40, "normal")
-sc = trtl.Turtle()
+btn_font = ("Times New Roman", 20, "bold")
+
+sc = trtl.Turtle(visible=False)
 sc.pu()
-sc.hideturtle()
 sc.goto(0, 300)
 
-score_writer = trtl.Turtle()
-score_writer.pu()
-score_writer.hideturtle()
-score_writer.goto(-295, 230)
-
-#-------preparing for code-------
-global click
-global answer
-global v1
-global v2
-global birdfall_type
-boolz = True
-bool2 = True
-temp = True
-click = 0
-v1 = 0
-v2 = 0
-birdfall_type = redbirdfall
-answer = 0
-global choose_time
-choose_time = True
-global done
-done = True
-global score
-score = 0
-time = 0
-y_vel = 0
-ycord = 0
-distance = 400
-
-p = trtl.Turtle()
-p2 = trtl.Turtle()
-p3 = trtl.Turtle()
-p4 = trtl.Turtle()
-p5 = trtl.Turtle()
-p6 = trtl.Turtle()
-p.pu()
-p2.pu()
-p3.pu()
-p4.pu()
-p5.pu()
-p6.pu()
-p.shape(pipe2)
-p2.shape(pipe)
-p3.shape(pipe2)
-p4.shape(pipe)
-p5.shape(pipe2)
-p6.shape(pipe)
+bird = trtl.Turtle()
 bird.pu()
-bird_type = redbird
+bird.ht()  # Hide default black arrow
+bird.speed(0)
 
-p.hideturtle()
-p2.hideturtle()
-p3.hideturtle()
-p4.hideturtle()
-p5.hideturtle()
-p6.hideturtle()
+easy_btn = trtl.Turtle(shape="square");
+easy_btn.color("lightgreen");
+easy_btn.shapesize(2, 6);
+easy_btn.pu();
+easy_btn.ht()
+med_btn = trtl.Turtle(shape="square");
+med_btn.color("gold");
+med_btn.shapesize(2, 6);
+med_btn.pu();
+med_btn.ht()
+hard_btn = trtl.Turtle(shape="square");
+hard_btn.color("tomato");
+hard_btn.shapesize(2, 6);
+hard_btn.pu();
+hard_btn.ht()
 
-b0 = trtl.Turtle()
-b0.pu()
-b0.shape(back2)
+click = 0
+score = 0
+y_vel = 0
+bird_type = "redbird.gif"
+birdfall_type = "redbirdfall.gif"
+game_active = False
+pipeSpeed = 25
+pipe_gap = 800
+current_mode = "medium"
 
-b1 = trtl.Turtle()
-b1.pu()
-b2 = trtl.Turtle()
-b2.pu()
-b3 = trtl.Turtle()
-b3.pu()
+p_list = []
+for i in range(3):
+    tp = trtl.Turtle(shape="pipe2.gif")
+    bp = trtl.Turtle(shape="pipe.gif")
+    for p in [tp, bp]:
+        p.pu()
+        p.ht()
+    p_list.append([tp, bp, 0, False])
 
-box = trtl.Turtle()
-box.hideturtle()
-
-def updateHeight(num):
-  global height
-  global height2
-  global height3
-  if num == 1:
-    height = ran.randint(350, 720)
-    wn.update()
-    return height
-  elif num == 2:
-    height2 = ran.randint(350, 720)
-    wn.update()
-    return height2
-  elif num == 3:
-    height3 = ran.randint(350, 720)
-    wn.update()
-    return height3
-
-
-#-------Choosing a bird-------
-def choose_bird():
-  global bool2
-  while (bool2):
-    sc.goto(-130, 150)
-    sc.write("Pick A Bird", font=font_setup)
-    b1.goto(-200, 50)
-    b1.shape(yellowbird)
-    b2.goto(0, 50)
-    b2.shape(bluebird)
-    b3.goto(200, 50)
-    b3.shape(redbird)
-    bool2 = False
+b1, b2, b3 = trtl.Turtle(), trtl.Turtle(), trtl.Turtle()
+for b in [b1, b2, b3]:
+    b.pu()
+    b.ht()
 
 
-def yellow_dos(x, y):
-  sc.goto(0, 300)
-  sc.clear()
-  global click
-  global answer
-  global bird_type
-  global birdfall_type
-  bird_type = yellowbirdup
-  birdfall_type = yellowbirdfall
-  b1.hideturtle()
-  b2.hideturtle()
-  b3.hideturtle()
-  answer == 1
-  click += 1
-  choose_time == False
+# -------Functions-------
+def updateHeight(old_h=None):
+    """Generates a height that is at least 150 pixels away from the previous one."""
+    new_h = ran.randint(350, 720)
+    if old_h is not None:
+        # Keep picking a new height until the jump is significant enough
+        while abs(new_h - old_h) < 150:
+            new_h = ran.randint(350, 720)
+    return new_h
 
 
-def blue_dos(x, y):
-  sc.goto(0, 300)
-  sc.clear()
-  global click
-  global answer
-  global bird_type
-  global birdfall_type
-  bird_type = bluebirdup
-  birdfall_type = bluebirdfall
-  b1.hideturtle()
-  b2.hideturtle()
-  b3.hideturtle()
-  answer == 2
-  click += 1
-  choose_time == False
-
-
-def red_dos(x, y):
-  sc.goto(0, 300)
-  sc.clear()
-  global click
-  global answer
-  global bird_type
-  global birdfall_type
-  bird_type = redbirdup
-  birdfall_type = redbirdfall
-  b1.hideturtle()
-  b2.hideturtle()
-  b3.hideturtle()
-  answer == 3
-  click += 1
-  choose_time == False
-
-
-#-------bird falling and going up-------
-def run_gravity():
-  global birdfall_type
-  global ycord
-  ycord = bird.ycor()
-  global y_vel
-  bird.goto(bird.xcor(), bird.ycor() + y_vel)
-  y_vel -= 5
-  global v1
-  global v2
-  if (answer == 1):
-    v1 = 27
-    v2 = 32
-  if (answer == 2):
-    v1 = 35
-    v2 = 33
-  if (answer == 3):
-    v1 = 32
-    v2 = 28
-  if y_vel <= -20:
-    global birdfall_type
-    bird.shape(birdfall_type)
-    bird.update()
-  else:
-    bird.shape(bird_type)
-    bird.update()
-  if (ycord > -240):
-    bird.ontimer(run_gravity, 50)
-  else:
-    bird.goto(-400, -250)
-    bird.update()
-    bird.shape(birdfall_type)
-    times.sleep(0.5)
-    bird.hideturtle()
-    wn.update()
-
-
-def mouse_going_up(x, y):
-  global v1
-  global v2
-  if (answer == 1):
-    v1 = 28
-    v2 = 27
-  if (answer == 2):
-    v1 = 28
-    v2 = 25
-  if (answer == 3):
-    v1 = 26
-    v2 = 26
-  global ycord
-  ycord = bird.ycor()
-  global y_vel
-  y_vel = 0
-  bird.goto(bird.xcor(), bird.ycor() + y_vel)
-  y_vel += 23
-  bird.update()
-
-
-def going_up():
-  global v1
-  global v2
-  if (answer == 1):
-    v1 = 28
-    v2 = 27
-  if (answer == 2):
-    v1 = 28
-    v2 = 25
-  if (answer == 3):
-    v1 = 26
-    v2 = 26
-  global ycord
-  ycord = bird.ycor()
-  global y_vel
-  y_vel = 0
-  bird.goto(bird.xcor(), bird.ycor() + y_vel)
-  y_vel += 19
-  bird.update()
-
-#-------checking if the bird collides with every indvidual pipe-------
-def pipe_collison():
-  global v1
-  global v2
-  if (bird.xcor() + 30 == p.xcor()):
-    if ((bird.ycor() + v1 <= height - 380
-         and bird.ycor() - v2 >= height - 510)):
-      global score
-      sc.clear()
-      score += 1
-      sc.write(score, font=font_setup)
-      if score == score:
-        box.clear()
-      wn.update()
-    else:
-      bird.hideturtle()
-      bird.shape(explosion)
-      bird.stamp()
-      stop_pipes()
-      wn.update()
-      game_over(score)
-      times.sleep(0.3)
-      bird.clear()
-
-  if (bird.xcor() + 30 == p3.xcor()):
-    if (bird.ycor() + v1 <= height2 - 380
-        and bird.ycor() - v2 >= height2 - 510):
-      sc.clear()
-      score += 1
-      sc.write(score, font=font_setup)
-      if score == score:
-        box.clear()
-        
-      wn.update()
-    else:
-      bird.hideturtle()
-      bird.shape(explosion)
-      bird.stamp()
-      stop_pipes()
-      wn.update()
-      game_over(score)
-      times.sleep(0.3)
-      bird.clear()
-
-  if (bird.xcor() + 30 == p5.xcor() + 23):
-    if (bird.ycor() + v1 <= height3 - 380
-        and bird.ycor() - v2 >= height3 - 520):
-      sc.clear()
-      score += 1
-      sc.write(score, font=font_setup)
-      if score == score:
-        box.clear()
-      wn.update()
-    else:
-      bird.hideturtle()
-      bird.shape(explosion)
-      bird.stamp()
-      stop_pipes()
-      wn.update()
-      game_over(score)
-      times.sleep(0.3)
-      bird.clear()
-
-
-#-------stopping pipes-------
-def stop_pipes():
-  p.goto(-700, p.ycor())
-  p2.goto(-700, p2.ycor())
-  p.hideturtle()
-  p2.hideturtle()
-  p3.goto(-700, p3.ycor())
-  p4.goto(-700, p4.ycor())
-  p4.hideturtle()
-  p3.hideturtle()
-  p5.goto(-700, p5.ycor())
-  p6.goto(-700, p6.ycor())
-  p5.hideturtle()
-  p6.hideturtle()
-  wn.update()
-
-
-#-------drawing and moving the pipes-------
-def move_pipe():
-  height = updateHeight(1)
-  height2 = updateHeight(2)
-  height3 = updateHeight(3)
-  p.goto(800, height)
-  p2.goto(800, height - 950)
-  p3.goto(1300, height2)
-  p4.goto(1300, height2 - 950)
-  p5.goto(1800, height3)
-  p6.goto(1800, height3 - 950)
-  p.showturtle()
-  p2.showturtle()
-  p3.showturtle()
-  p4.showturtle()
-  p5.showturtle()
-  p6.showturtle()
-
-  while True:
-    if (bird.ycor() == -250):
-      stop_pipes()
-      game_over(score)
-    else:
-      if p.xcor() <= -650 or p2.xcor() <= -650:
-        height = updateHeight(1)
-        p.goto(900, height)
-        p2.goto(900, height - 950)
-      else:
-        p.backward(pipeSpeed)
-        p2.backward(pipeSpeed)
-      if p3.xcor() <= -650 or p4.xcor() <= -650:
-        height2 = updateHeight(2)
-        p3.goto(900, height2)
-        p4.goto(900, height2 - 950)
-      else:
-        p3.backward(pipeSpeed)
-        p4.backward(pipeSpeed)
-      if p5.xcor() <= -650 or p6.xcor() <= -650:
-        height3 = updateHeight(3)
-        p5.goto(900, height3)
-        p6.goto(900, height3 - 950)
-      else:
-        p5.backward(pipeSpeed)
-        p6.backward(pipeSpeed)
-      pipe_collison()
-      wn.update()
-      box.clear()
-
-
-def game_over(score):
-  global bool
-  global boolz
-  while (bool == True):
-    bird.goto(-400, -250)
+def init_difficulty():
+    bird.ht()
     sc.clear()
-    sc.goto(-270, 240)
-    sc.write("Game Over", font=font_setup2)
-    sc.goto(-190, 160)
-    sc.write("Final Score:", font=font_setup3)
-    sc.goto(160, 160)
-    sc.write(score, font=font_setup3)
-    box.clear()
-    bool = False
+    sc.goto(0, 180)
+    sc.write("Select Difficulty", align="center", font=font_setup)
+    easy_btn.goto(-200, 50);
+    easy_btn.st()
+    sc.goto(-200, 0);
+    sc.write("EASY", align="center", font=btn_font)
+    med_btn.goto(0, 50);
+    med_btn.st()
+    sc.goto(0, 0);
+    sc.write("MEDIUM", align="center", font=btn_font)
+    hard_btn.goto(200, 50);
+    hard_btn.st()
+    sc.goto(200, 0);
+    sc.write("HARD", align="center", font=btn_font)
+    easy_btn.onclick(lambda x, y: set_difficulty(15, 1000, "easy"))
+    med_btn.onclick(lambda x, y: set_difficulty(25, 800, "medium"))
+    hard_btn.onclick(lambda x, y: set_difficulty(35, 650, "hard"))
+    wn.update()
 
-def starter():
-  global temp
-  if (click == 0 and temp == True):
-    bird.goto(-400, 150)
-    bird.hideturtle()
-    bird.update()
-    if (choose_time == True):
-      choose_bird()
-      b1.onclick(yellow_dos)
-      b2.onclick(blue_dos)
-      b3.onclick(red_dos)
-      starter()
-    if click == 1:
-      temp = False
-      wn.update()
 
-starter()
+def set_difficulty(speed, gap, mode):
+    global pipeSpeed, pipe_gap, current_mode
+    pipeSpeed, pipe_gap, current_mode = speed, gap, mode
+    for btn in [easy_btn, med_btn, hard_btn]:
+        btn.ht()
+        btn.onclick(None)
+    init_selection()
 
-#moving the bird to its jumping spot and key press setup
-bird.hideturtle()
-bird.goto(-400, 250)
-bird.showturtle()
-bird.shape(bird_type)
-bird.onkeypress(going_up, "space")
-bird.onkeypress(going_up, "Up")
-bird.onkeypress(going_up, "w")
-bird.onscreenclick(mouse_going_up)
-run_gravity()
-bird.listen()
-sc.write(score, font=font_setup)
-bool = True
-  
-#if bird hits the ground the pipes stop otherwise the pipes move
-while (bool == True and click == 1):
-  move_pipe()
-if (bird.ycor() <= -250):
-  stop_pipes()
-  game_over(score)
-  bool = False
 
+def init_selection():
+    global click
+    click = 0
+    sc.clear()
+    sc.goto(0, 180)
+    sc.write("Pick A Bird", align="center", font=font_setup)
+    b1.goto(-200, 50);
+    b1.shape("yellowbird.gif");
+    b1.st()
+    b2.goto(0, 50);
+    b2.shape("bluebird.gif");
+    b2.st()
+    b3.goto(200, 50);
+    b3.shape("redbird.gif");
+    b3.st()
+    b1.onclick(lambda x, y: select_bird("yellowbirdup.gif", "yellowbirdfall.gif"))
+    b2.onclick(lambda x, y: select_bird("bluebirdup.gif", "bluebirdfall.gif"))
+    b3.onclick(lambda x, y: select_bird("redbirdup.gif", "redbirdfall.gif"))
+    wn.update()
+
+
+def select_bird(b_up, b_fall):
+    global bird_type, birdfall_type, click
+    bird_type, birdfall_type, click = b_up, b_fall, 1
+    sc.clear()
+    for b in [b1, b2, b3]: b.ht()
+    b1.onclick(None);
+    b2.onclick(None);
+    b3.onclick(None)
+    start_game_loop()
+
+
+def flap(x=None, y=None):
+    global y_vel, game_active
+    if game_active: y_vel = 19
+
+
+def handle_collision():
+    global game_active
+    if not game_active: return
+    game_active = False
+    bird.shape("explosion.gif")
+    wn.update()
+    times.sleep(0.5)
+    game_over()
+
+
+def game_over():
+    is_new_high = save_high_score(score, current_mode)
+    sc.clear()
+    sc.goto(0, 150)
+    sc.write("GAME OVER", align="center", font=font_setup2)
+    sc.goto(0, 50)
+    sc.write(f"Score: {score}", align="center", font=font_setup3)
+    sc.goto(0, -10)
+    sc.write(f"High Score ({current_mode.capitalize()}): {get_high_score(current_mode)}", align="center",
+             font=font_setup3)
+    if is_new_high:
+        sc.goto(0, -70)
+        sc.write("NEW RECORD!", align="center", font=("Times New Roman", 30, "bold"))
+    sc.goto(0, -150)
+    sc.write("Press 'R' to Play Again", align="center", font=font_setup)
+    wn.onkeypress(reset_game, "r")
+    wn.onkeypress(reset_game, "R")
+    wn.update()
+
+
+def reset_game():
+    if not game_active:
+        wn.onkeypress(None, "r")
+        wn.onkeypress(None, "R")
+        sc.clear()
+        init_difficulty()
+
+
+def game_tick():
+    global y_vel, game_active, score
+    if not game_active: return
+
+    bird.goto(bird.xcor(), bird.ycor() + y_vel)
+    y_vel -= 3
+    bird.shape(birdfall_type if y_vel <= -15 else bird_type)
+
+    if bird.ycor() <= -260:
+        handle_collision()
+        return
+
+    for p_pair in p_list:
+        top, bot, h, passed = p_pair
+        top.bk(pipeSpeed)
+        bot.bk(pipeSpeed)
+
+        # Collision Check
+        if abs(bird.xcor() - top.xcor()) < 55:
+            if bird.ycor() + 12 >= h - 380 or bird.ycor() - 12 <= h - 510:
+                handle_collision()
+                return
+
+        if bird.xcor() > top.xcor() and not passed:
+            score += 1
+            p_pair[3] = True
+            sc.clear()
+            sc.goto(0, 300)
+            sc.write(score, align="center", font=font_setup)
+
+        # Reset Pipe with New Randomized Logic
+        if top.xcor() < -1200:
+            old_h = p_pair[2]
+            new_h = updateHeight(old_h)  # Pass the old height to ensure randomness
+            p_pair[2], p_pair[3] = new_h, False
+            new_x = top.xcor() + (pipe_gap * 3)
+            top.goto(new_x, new_h)
+            bot.goto(new_x, new_h - 950)
+
+    wn.update()
+    wn.ontimer(game_tick, 40)
+
+
+def start_game_loop():
+    global score, y_vel, game_active
+    score, y_vel, game_active = 0, 0, True
+    bird.goto(-300, 0);
+    bird.st()
+
+    # Initialize pipes with significant height differences
+    last_h = 500
+    for i, p_pair in enumerate(p_list):
+        h = updateHeight(last_h)
+        last_h = h
+        p_pair[2], p_pair[3] = h, False
+        p_pair[0].goto(800 + (i * pipe_gap), h)
+        p_pair[1].goto(800 + (i * pipe_gap), h - 950)
+        p_pair[0].st();
+        p_pair[1].st()
+
+    sc.goto(0, 300)
+    sc.write(score, align="center", font=font_setup)
+    game_tick()
+
+
+wn.onkeypress(flap, "space")
+wn.onscreenclick(flap)
+wn.listen()
+init_difficulty()
 wn.mainloop()
